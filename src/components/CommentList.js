aa93e03ca755eed53b34e9a2fa4a7f74d7fe5665
Darryl <darrylmangibin@gmail.com>
2019-06-01 20:10:53 +0800

    enzyme setup

---
+++ src/components/CommentList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComponentList extends Component {
+  render() { 
+    return (
+      <div>
+        Comment list
+      </div>
+    );
+  }
+}
+ 
+export default ComponentList;
